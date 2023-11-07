ccae8d5d90a47cab19094d784ab111e1a30616c9
Bernard Borio <bernardborio@gmail.com>
2023-11-07 22:25:57 +0100

    Student page skeleton created

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from "@angular/router";
+import { StudentPageComponent } from './pages/student-page/student-page.component';
+
+const routes: Routes = [{path: 'student', component: StudentPageComponent}]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
