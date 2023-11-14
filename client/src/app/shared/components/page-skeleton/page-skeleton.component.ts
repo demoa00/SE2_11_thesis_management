30f59df640217dd02fd9cfc8395e81fa18468d86
Bernard Borio <bernardborio@gmail.com>
2023-11-14 18:11:53 +0100

    Added logout button

--- client/src/app/shared/components/page-skeleton/page-skeleton.component.ts
+++ client/src/app/shared/components/page-skeleton/page-skeleton.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-page-skeleton',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class PageSkeletonComponent {
 
+  constructor(private _router: Router) {}
+  logout() {
+    this._router.navigate(['/']);
+  }
 }
