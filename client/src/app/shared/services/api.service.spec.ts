f5eda23fcfd351da302b770a9beeac4ca185b05c
Simone Bugni Duch <simonebugniduch.SBD@gmail.com>
2023-11-16 15:05:46 +0100

    minor fix

--- client/src/app/shared/services/api.service.spec.ts
+++ client/src/app/shared/services/api.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { APIService } from './api.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('APIService', () => {
   let service: APIService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule]
+    });
     service = TestBed.inject(APIService);
   });
 
