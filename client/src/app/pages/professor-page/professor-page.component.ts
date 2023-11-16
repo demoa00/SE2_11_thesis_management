173eb7aed88f871f611711ac7e511268d4a01646
morac <moracci99@gmail.com>
2023-11-16 16:23:03 +0100

    minor fix

--- client/src/app/pages/professor-page/professor-page.component.ts
+++ client/src/app/pages/professor-page/professor-page.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class ProfessorPageComponent {
 
+  menuItems = [
+    { id: 1, hover: false, selected: true  },
+    { id: 2, hover: false, selected: false },
+    { id: 3, hover: false, selected: false }
+  ];
+  selectMenuItem(id: number) {
+    this.menuItems.forEach(item => {
+      item.selected = item.id === id;
+    });
+  }
+
 }
