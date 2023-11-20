3ff5cdadfce8ddf9f105a30637e491c1e7c78299
eric.yugo28@gmail.com <erichioki@Yugos-MacBook-Pro.local>
2023-11-20 14:07:42 +0100

    add Keyword tests

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -1,48 +1,61 @@
-'use strict';
+"use strict";
 
-var passport = require('passport');
-var LocalStrategy = require('passport-local');
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
 
-var utils = require('../utils/writer.js');
-var User = require('../service/UserService');
+var utils = require("../utils/writer.js");
+var User = require("../service/UserService");
 
-// Set up local strategy to verify, search in the DB a user with a matching password, 
+// Set up local strategy to verify, search in the DB a user with a matching password,
 // and retrieve its information (i.e., id, username, name).
-passport.use(new LocalStrategy(
-  function (username, password, done) {
+passport.use(
+  new LocalStrategy(function (username, password, done) {
     User.createNewAuthenticatedSession(username, password).then((user) => {
       if (!user) {
-        return done(null, false, { message: 'Incorrect username and/or password.' });
+        return done(null, false, {
+          message: "Incorrect username and/or password.",
+        });
       } else {
-        User.getUserData(user).then((userData) => {
-          return done(null, userData);
-        }).catch(() => { return done(null, false, { message: 'Incorrect username and/or password.' }); });
+        User.getUserData(user)
+          .then((userData) => {
+            return done(null, userData);
+          })
+          .catch(() => {
+            return done(null, false, {
+              message: "Incorrect username and/or password.",
+            });
+          });
       }
-    })
-  }
-));
+    });
+  })
+);
 
-module.exports.createNewAuthenticatedSession = function createNewAuthenticatedSession(req, res, next) {
-  passport.authenticate('local', (err, user, info) => {
-    if (err) {
-      return next(err);
-    }
-
-    if (!user) {
-      utils.writeJson(res, info, 400);
-    }
-
-    req.login(user, (err) => {
+module.exports.createNewAuthenticatedSession =
+  function createNewAuthenticatedSession(req, res, next) {
+    passport.authenticate("local", (err, user, info) => {
       if (err) {
         return next(err);
       }
 
-      utils.writeJson(res, req.user, 201);
-    });
-  })(req, res, next);
-};
+      if (!user) {
+        utils.writeJson(res, info, 400);
+      }
 
-module.exports.deleteAuthenticatedSession = function deleteAuthenticatedSession(req, res, next) {
+      req.login(user, (err) => {
+        if (err) {
+          return next(err);
+        }
+
+        utils.writeJson(res, req.user, 201);
+      });
+    })(req, res, next);
+  };
+
+module.exports.deleteAuthenticatedSession = function deleteAuthenticatedSession(
+  req,
+  res,
+  next
+) {
   if (req.user.isStudent) {
     if (req.user.studentId === req.params.userId) {
       req.logOut(() => res.end());
