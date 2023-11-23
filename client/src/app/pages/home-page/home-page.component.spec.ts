320e2fdd73b9a3b9ba135b59b59dc0dde9520092
Simone Bugni Duch <simonebugniduch.SBD@gmail.com>
2023-11-23 14:55:27 +0100

    Test for HomePageComponent and LoginFormComponent

--- client/src/app/pages/home-page/home-page.component.spec.ts
+++ client/src/app/pages/home-page/home-page.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { HomePageComponent } from './home-page.component';
 import { ButtonComponent } from 'src/app/shared/components/button/button.component';
@@ -8,16 +8,30 @@ import { PopupComponent } from 'src/app/shared/components/popup-conferma/popup.c
 import { AlertComponent } from 'src/app/shared/alert/alert.component';
 import { LoginFormComponent } from 'src/app/shared/components/login-form/login-form.component';
 import { HttpClientModule } from '@angular/common/http';
+import { of } from 'rxjs';
+import { AuthService } from '@auth0/auth0-angular';
 
 describe('HomePageComponent', () => {
   let component: HomePageComponent;
   let fixture: ComponentFixture<HomePageComponent>;
 
-  beforeEach(() => {
+  const authServiceMock = {
+    isAuthenticated$: of(true),
+    loginWithRedirect: () => {},
+    logout: () => {}
+  };
+
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
+      declarations: [HomePageComponent, ButtonComponent, PageSkeletonComponent, PopupComponent, AlertComponent, LoginFormComponent],
       imports: [FormsModule, HttpClientModule],
-      declarations: [HomePageComponent, ButtonComponent, PageSkeletonComponent, PopupComponent, AlertComponent, LoginFormComponent]
-    });
+      providers: [
+        { provide: AuthService, useValue: authServiceMock },
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(HomePageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- client/src/app/shared/components/login-form/login-form.component.spec.ts
+++ client/src/app/shared/components/login-form/login-form.component.spec.ts
@@ -1,19 +1,32 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { LoginFormComponent } from './login-form.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ButtonComponent } from '../button/button.component';
 import { FormsModule } from '@angular/forms';
+import { of } from 'rxjs';
+import { AuthService } from '@auth0/auth0-angular';
 
 describe('LoginFormComponent', () => {
   let component: LoginFormComponent;
   let fixture: ComponentFixture<LoginFormComponent>;
 
-  beforeEach(() => {
+  const authServiceMock = {
+    loginWithRedirect: () => {},
+    user$: of({})
+  };
+
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule, FormsModule],
-      declarations: [LoginFormComponent, ButtonComponent]
-    });
+      declarations: [LoginFormComponent, ButtonComponent],
+      providers: [
+        { provide: AuthService, useValue: authServiceMock }
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(LoginFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
