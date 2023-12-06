5cfbb33c2f3e9b26b3e62beb7d9072e2138cc5f3
Simone Bugni Duch <simonebugniduch.SBD@gmail.com>
2023-12-06 16:38:13 +0100

    ButtonSliderComponent tests

--- client/src/app/shared/components/button-slider/button-slider.component.spec.ts
+++ client/src/app/shared/components/button-slider/button-slider.component.spec.ts
@@ -1,56 +1,71 @@
-import { ComponentFixture, TestBed, fakeAsync, flushMicrotasks, tick } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick, flush } from '@angular/core/testing';
 import { ButtonSliderComponent } from './button-slider.component';
 import { IconComponent } from '../icon/icon.component';
 import { CdkDragEnd, CdkDragMove } from '@angular/cdk/drag-drop';
 
 describe('ButtonSliderComponent', () => {
-    let component: ButtonSliderComponent;
-    let fixture: ComponentFixture<ButtonSliderComponent>;
+  let component: ButtonSliderComponent;
+  let fixture: ComponentFixture<ButtonSliderComponent>;
 
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            declarations: [ButtonSliderComponent, IconComponent]
-        });
-        fixture = TestBed.createComponent(ButtonSliderComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    });
+  beforeEach(() => {
+      TestBed.configureTestingModule({
+          declarations: [ButtonSliderComponent, IconComponent]
+      });
+      fixture = TestBed.createComponent(ButtonSliderComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+  });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
-    });
+  it('should create', () => {
+      expect(component).toBeTruthy();
+  });
 
-    it('should update messageSendable property on drag', () => {
-        const dragMoveEvent: CdkDragMove = {
-          source: {
-            getFreeDragPosition: () => ({ x: 114, y: 0 }),
-          },
-        } as any;
+  it('should update messageSendable property on drag', () => {
+      const dragMoveEvent: CdkDragMove = {
+        source: {
+          getFreeDragPosition: () => ({ x: 114, y: 0 }),
+        },
+      } as any;
     
-        component.takePosition(dragMoveEvent);
-    
-        expect(component.messageSendable).toBe(true);
-    });
+      component.takePosition(dragMoveEvent);
+  
+      expect(component.messageSendable).toBe(true);
+  });
 
-    it('should emit buttonSliderSubmit event when message is sendable', () => {
-        spyOn(component.buttonSliderSubmit, 'emit');
-    
-        const dragMoveEvent: CdkDragMove = {
-          source: {
-            getFreeDragPosition: () => ({ x: 114, y: 0 }),
-          },
-        } as any;
-    
-        component.takePosition(dragMoveEvent);
-    
-        const dragEndEvent: CdkDragEnd = {
-          source: {
-            setFreeDragPosition: () => {},
-          },
-        } as any;
-    
-        component.checkDragEnd(dragEndEvent);
-    
-        expect(component.buttonSliderSubmit.emit).toHaveBeenCalledWith(true);
-    });
+  it('should emit buttonSliderSubmit event when message is sendable', fakeAsync(() => {
+    spyOn(component.buttonSliderSubmit, 'emit');
+
+    const dragMoveEvent: CdkDragMove = {
+      source: {
+        getFreeDragPosition: () => ({ x: 114, y: 0 }),
+      },
+    } as any;
+
+    component.takePosition(dragMoveEvent);
+
+    const dragEndEvent: CdkDragEnd = {
+      source: {
+        setFreeDragPosition: () => {},
+      },
+    } as any;
+
+    component.checkDragEnd(dragEndEvent);
+
+    tick(1999); // Use 1999 instead of 2000 to avoid reaching the exact time point
+    flush();
+
+    expect(component.buttonSliderSubmit.emit).toHaveBeenCalledWith(true);
+  }));
+
+  it('should set messageSendable to false when x is less than 113', () => {
+    const dragMoveEvent: CdkDragMove = {
+      source: {
+        getFreeDragPosition: () => ({ x: 112, y: 0 }),
+      },
+    } as any;
+
+    component.takePosition(dragMoveEvent);
+
+    expect(component.messageSendable).toBe(false);
+  });
 });
