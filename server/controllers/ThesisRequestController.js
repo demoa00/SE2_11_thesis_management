186a9c9ec6f12301e1825c3385c67393f9be0e67
AlessandroBelasri <belalessandro1@gmail.com>
2023-12-08 12:08:48 +0100

    ThesisRequestController done, checkRole updated with SC role

--- server/controllers/ThesisRequestController.js
+++ server/controllers/ThesisRequestController.js
@@ -5,17 +5,53 @@ const ThesisRequest = require("../service/ThesisRequestService");
 const checkRole = require("../utils/checkRole.js");
 
 module.exports.getThesisRequests = async function getThesisRequests(req, res, next) {
-    //to do 
+    try {
+        let thesisRequestList;
+        if (checkRole.isProfessor(req.user)) {  // get the requests for his own thesis
+            thesisRequestList = await ThesisRequest.getThesisRequestsForProfessor(req.user.userId);
+            utils.writeJson(res, thesisRequestList, 200);
+        } else if (checkRole.isSecretaryClerck(req.user)) { // get all the requests 
+            thesisRequestList = await ThesisRequest.getThesisRequestsForSecretary();
+            utils.writeJson(res, thesisRequestList, 200);
+        } else {
+            utils.writeJson(res, { error: "Forbidden" }, 403);
+        }
+    } catch (error) {
+        utils.writeJson(res, { error: error.message }, error.code);
+    }
 };
 
-module.exports.insertNewThesisRequest = async function insertNewThesisRequest(req, res, next){
-    //to do
+module.exports.insertNewThesisRequest = async function insertNewThesisRequest(req, res, next) {
+    try {
+        let newThesisRequest = await ThesisRequest.insertNewThesisRequest(req.user.userId, req.body);
+        if (newThesisRequest instanceof PromiseError) {
+            utils.writeJson(res, { error: newThesisRequest.message }, newThesisRequest.code);
+        } else {
+            utils.writeJson(res, newThesisRequest, 200);
+        }
+    } catch (error) {
+        utils.writeJson(res, { error: error.message }, error.code);
+    }
 };
 
-module.exports.getThesisRequestsById = async function getThesisRequestById(req, res, next){
-    //to do
+module.exports.getThesisRequestById = async function getThesisRequestById(req, res, next) {
+    try {
+        let thesisRequest = await ThesisRequest.getThesisRequestById(req.user, req.params.thesisRequestId);
+        utils.writeJson(res, thesisRequest, 200);
+    } catch (error) {
+        utils.writeJson(res, { error: error.message }, error.code);
+    }
 };
 
-module.exports.updateThesisRequest = async function updateThesisRequest(req, res, next){
-    //to do
+module.exports.updateThesisRequest = async function updateThesisRequest(req, res, next) {
+    try {
+        let thesisRequestUpdated = await ThesisRequest.updateThesisRequest(req.user.userId, req.body, req.params.thesisRequestId);
+        if (thesisRequestUpdated instanceof PromiseError) {
+            utils.writeJson(res, { error: thesisRequestUpdated.message }, thesisRequestUpdated.code);
+        } else {
+            utils.writeJson(res, thesisRequestUpdated, 200);
+        }
+    } catch (error) {
+        utils.writeJson(res, { error: error.message }, error.code);
+    }
 };
