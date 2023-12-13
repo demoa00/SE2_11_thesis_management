8ca2f11fa0e6b177b2eec5819f21a2e2cdaf2b03
Simone Bugni Duch <simonebugniduch.SBD@gmail.com>
2023-12-13 18:53:25 +0100

    PageSkeletonComponent tests

--- client/src/app/shared/components/page-skeleton/page-skeleton.component.spec.ts
+++ client/src/app/shared/components/page-skeleton/page-skeleton.component.spec.ts
@@ -5,22 +5,102 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ButtonComponent } from '../button/button.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MatIconModule } from '@angular/material/icon';
+import { APIService } from '../../services/api.service';
 
 describe('PageSkeletonComponent', () => {
   let component: PageSkeletonComponent;
   let fixture: ComponentFixture<PageSkeletonComponent>;
+  let apiService: jasmine.SpyObj<APIService>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    apiService = jasmine.createSpyObj('APIService', {
+      'logout': null,
+      'checkAutorization': Promise.resolve([]),
+      'getUserDetails': Promise.resolve({}),
+      'getProfessorDetails': Promise.resolve([])
+    });
+
+    await TestBed.configureTestingModule({
       imports: [RouterTestingModule, HttpClientTestingModule, MatIconModule],
       declarations: [PageSkeletonComponent, ButtonComponent],
-    });
+      providers: [{ provide: APIService, useValue: apiService }]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(PageSkeletonComponent);
     component = fixture.componentInstance;
+
+    await fixture.whenStable();
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call APIService.logout() when logout() is called', () => {
+    const user = { userId: 'testUserId', role: 'student' };
+    spyOn(localStorage, 'getItem').and.returnValue(JSON.stringify(user));
+    spyOn(localStorage, 'removeItem');
+
+    component.logout();
+
+    expect(apiService.logout).toHaveBeenCalledWith(user.userId);
+    expect(localStorage.removeItem).toHaveBeenCalledWith('user');
+  });
+
+  it('should call getUserDetails when user role is student', async () => {
+    const user = { userId: 'testUserId', role: 'student' };
+    spyOn(localStorage, 'getItem').and.returnValue(JSON.stringify(user));
+  
+    component.ngOnInit();
+  
+    expect(apiService.getUserDetails).toHaveBeenCalledWith(user.userId);
+  });
+
+  it('should call getProfessorDetails when user role is professor', async () => {
+    const user = { userId: 'testUserId', role: 'professor' };
+    spyOn(localStorage, 'getItem').and.returnValue(JSON.stringify(user));
+  
+    component.ngOnInit();
+  
+    expect(apiService.getProfessorDetails).toHaveBeenCalledWith(user.userId);
+  });
+
+  it('should set menuOpen to true when openMenu() is called', () => {
+    component.menuOpen = false;
+
+    component.openMenu();
+
+    expect(component.openMenu).toBeTruthy();
+  });
+
+  it('should call goToProfile() and emit true to profilePage', () => {
+    const profilePageSpy = spyOn(component.profilePage, 'emit');
+
+    component.goToProfile();
+
+    expect(profilePageSpy).toHaveBeenCalledWith(true);
+  });
+
+  it('should open notifications and emit event', fakeAsync(() => {
+    spyOn(component.notificationsOpenChange, 'emit');
+  
+    component.openNotifications();
+  
+    tick();
+  
+    expect(component.notificationsOpen).toBe(true);
+    expect(component.notificationsOpenChange.emit).toHaveBeenCalledWith(true);
+  }));
+
+  it('should close notifications', () => {
+    component.notificationsOpen = true;
+    const emitSpy = spyOn(component.notificationsOpenChange, 'emit');
+  
+    component.closeNotifications();
+  
+    expect(component.notificationsOpen).toBe(false);
+    expect(emitSpy).toHaveBeenCalledWith(false);
+  });  
 });
