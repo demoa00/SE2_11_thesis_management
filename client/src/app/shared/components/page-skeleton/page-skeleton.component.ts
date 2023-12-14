2fe8a9ba0b1bd98b8836ab698390a0bf8382a084
Bernard Borio <bernardborio@gmail.com>
2023-12-14 11:59:38 +0100

    Minor fix

--- client/src/app/shared/components/page-skeleton/page-skeleton.component.ts
+++ client/src/app/shared/components/page-skeleton/page-skeleton.component.ts
@@ -31,7 +31,8 @@ import {MatDatepickerInputEvent} from "@angular/material/datepicker";
 
 export class PageSkeletonComponent {
 
-  constructor(private _router: Router, private api: APIService, private darkMode: DarkModeService) {}
+  constructor(private _router: Router, private api: APIService, private darkMode: DarkModeService) {
+  }
 
   currentRoute = this._router.url;
 
@@ -40,6 +41,7 @@ export class PageSkeletonComponent {
   @Input() notificationsOpen = false
   @Output() profilePage = new EventEmitter<boolean>();
   @Output() notificationsOpenChange = new EventEmitter<boolean>();
+  @Output() newProposals = new EventEmitter<any>();
 
   user: User | undefined;
   student: StudentDetails | undefined;
@@ -47,23 +49,23 @@ export class PageSkeletonComponent {
 
   theme = false
   menuOpen = false
+  today = new Date()
 
   logout() {
-    let user= localStorage.getItem('user')
+    let user = localStorage.getItem('user')
     localStorage.removeItem('user')
-    this.api.logout(JSON.parse(user!=null?user:'').userId);
+    this.api.logout(JSON.parse(user != null ? user : '').userId);
   }
 
-  ngOnInit(){
+  ngOnInit() {
     this.api.checkAutorization()
     this.user = JSON.parse(localStorage.getItem('user') || '{}')
-    if(this.user?.role=='student'){
-      this.api.getUserDetails(this.user?.userId).then((response: any)=>{
+    if (this.user?.role == 'student') {
+      this.api.getUserDetails(this.user?.userId).then((response: any) => {
         this.student = response
       })
-    }
-    else if(this.user?.role=='professor'){
-      this.api.getProfessorDetails(this.user?.userId).then((response: any)=>{
+    } else if (this.user?.role == 'professor') {
+      this.api.getProfessorDetails(this.user?.userId).then((response: any) => {
         console.log(response)
         this.professor = response
       })
@@ -97,15 +99,22 @@ export class PageSkeletonComponent {
     this.notificationsOpenChange.emit(this.notificationsOpen)
   }
 
-    selectDate($event: MatDatepickerInputEvent<any, any>) {
-      let year = $event.value.getFullYear().toString()
-      let month = $event.value.getMonth() + 1 < 10 ? `0${$event.value.getMonth() + 1}` : ($event.value.getMonth() + 1).toString()
-      let day = $event.value.getDate() < 10 ? `0${$event.value.getDate()}` : $event.value.getDate().toString()
-      let date = `${year}-${month}-${day}`
-      this.api.putVirtualClock(date).then((response: any)=>{
+  selectDate($event: MatDatepickerInputEvent<any, any>) {
+    let year = $event.value.getFullYear().toString()
+    let month = $event.value.getMonth() + 1 < 10 ? `0${$event.value.getMonth() + 1}` : ($event.value.getMonth() + 1).toString()
+    let day = $event.value.getDate() < 10 ? `0${$event.value.getDate()}` : $event.value.getDate().toString()
+    let date = `${year}-${month}-${day}`
+    this.api.putVirtualClock(date).then((response: any) => {
+      console.log(response)
+      this.api.getAllProposals(null).then((response: any) => {
+        console.log("NEW PROPOSALS")
         console.log(response)
-      }).catch((error)=>{
+        this.newProposals.emit(response)
+      }).catch((error: any) => {
         console.log(error)
       })
-    }
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
 }
