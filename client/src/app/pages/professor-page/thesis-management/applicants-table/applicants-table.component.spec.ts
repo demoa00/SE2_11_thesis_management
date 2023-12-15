eeac9614dafedadede7552e3620951498716b680
Simone Bugni Duch <simonebugniduch.SBD@gmail.com>
2023-12-15 14:26:01 +0100

    Minor fix

--- client/src/app/pages/professor-page/thesis-management/applicants-table/applicants-table.component.spec.ts
+++ client/src/app/pages/professor-page/thesis-management/applicants-table/applicants-table.component.spec.ts
@@ -10,16 +10,23 @@ describe('ApplicantsTableComponent', () => {
   let fixture: ComponentFixture<ApplicantsTableComponent>;
   let apiServiceSpy: jasmine.SpyObj<APIService>;
 
-  beforeEach(() => {
-    const spy = jasmine.createSpyObj('APIService', ['putApplication']);
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    apiServiceSpy = jasmine.createSpyObj('APIService', {
+      'putApplication': Promise.resolve([]),
+      'getApplications': Promise.resolve([])
+    });
+
+    await TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       declarations: [ApplicantsTableComponent, IconComponent],
-      providers: [{ provide: APIService, useValue: spy }]
-    });
+      providers: [{ provide: APIService, useValue: apiServiceSpy }]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ApplicantsTableComponent);
     component = fixture.componentInstance;
-    apiServiceSpy = TestBed.inject(APIService) as jasmine.SpyObj<APIService>;
+
+    await fixture.whenStable();
+
     fixture.detectChanges();
   });
 
@@ -27,57 +34,108 @@ describe('ApplicantsTableComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should update filteredRows and set openFilter to false in ngOnChanges', () => {
+    const mockRows = [{}];
+    const initialFilteredRows = component.filteredRows;
+    const initialOpenFilter = component.openFilter;
+  
+    component.rows = mockRows;
+    component.ngOnChanges({ rows: { currentValue: mockRows, previousValue: undefined, isFirstChange: () => true, firstChange: true } });
+  
+    expect(component.filteredRows).toEqual(mockRows);
+    expect(component.openFilter).toBe(false);
+    expect(component.studentFilter).toBe('');
+    expect(component.titleFilter).toBe('');
+  });
+
   it('should open accept popup and reset state', () => {
+    const mockApplicant = { studentId: '123', thesisProposalId: '456' };
     component.acceptPopup = false;
     component.requestAccepted = true;
     component.response = { someData: 'test data' };
 
-    component.openAcceptPopup();
+    component.openAcceptPopup(mockApplicant);
 
     expect(component.acceptPopup).toBe(true);
     expect(component.requestAccepted).toBe(false);
     expect(component.response).toBeUndefined();
+    expect(component.applicant).toEqual(mockApplicant);
   });
 
   it('should open reject popup and reset state', () => {
+    const mockApplicant = { studentId: '123', thesisProposalId: '456' };
     component.rejectPopup = false;
     component.requestAccepted = false;
     component.response = { someData: 'test data' };
 
-    component.openRejectPopup();
+    component.openRejectPopup(mockApplicant);
 
     expect(component.rejectPopup).toBe(true);
     expect(component.requestAccepted).toBe(false);
     expect(component.response).toBeUndefined();
+    expect(component.applicant).toEqual(mockApplicant);
   });
 
-  // it('should reset applicant in rejectApplication', fakeAsync(() => {
-  //   const mockApplicant = { studentId: '123', thesisProposalId: '456' };
-  //   component.applicant = mockApplicant;
+  it('should reset applicant in rejectApplication', fakeAsync(() => {
+    const mockApplicant = { studentId: '123', thesisProposalId: '456' };
+    component.applicant = mockApplicant;
 
-  //   apiServiceSpy.putApplication.and.returnValue(Promise.resolve({}));
+    apiServiceSpy.putApplication.and.returnValue(Promise.resolve({}));
 
-  //   component.rejectApplication();
-  //   fixture.detectChanges();
-  //   tick();
+    component.rejectApplication();
+    fixture.detectChanges();
+    tick();
 
-  //   expect(apiServiceSpy.putApplication).toHaveBeenCalledWith(mockApplicant.studentId, mockApplicant.thesisProposalId, 'Rejected');
-  //   expect(component.requestAccepted).toBe(true);
-  //   expect(component.applicant).toBeUndefined();
-  // }));
+    expect(apiServiceSpy.putApplication).toHaveBeenCalledWith(mockApplicant.studentId, mockApplicant.thesisProposalId, 'Rejected');
+    expect(component.requestAccepted).toBe(true);
+    expect(component.applicant).toBeUndefined();
+  }));
 
-  // it('should reset applicant in acceptApplication', fakeAsync(() => {
-  //   const mockApplicant = { studentId: '123', thesisProposalId: '456' };
-  //   component.applicant = mockApplicant;
+  it('should reset applicant in acceptApplication', fakeAsync(() => {
+    const mockApplicant = { studentId: '123', thesisProposalId: '456' };
+    component.applicant = mockApplicant;
 
-  //   apiServiceSpy.putApplication.and.returnValue(Promise.resolve({}));
+    apiServiceSpy.putApplication.and.returnValue(Promise.resolve({}));
 
-  //   component.acceptApplication();
-  //   fixture.detectChanges();
-  //   tick();
+    component.acceptApplication();
+    fixture.detectChanges();
+    tick();
 
-  //   expect(apiServiceSpy.putApplication).toHaveBeenCalledWith(mockApplicant.studentId, mockApplicant.thesisProposalId, 'Accepted');
-  //   expect(component.requestAccepted).toBe(true);
-  //   expect(component.applicant).toBeUndefined();
-  // }));
+    expect(apiServiceSpy.putApplication).toHaveBeenCalledWith(mockApplicant.studentId, mockApplicant.thesisProposalId, 'Accepted');
+    expect(component.requestAccepted).toBe(true);
+    expect(component.applicant).toBeUndefined();
+  }));
+
+  it('should filter rows based on applied filters', () => {
+    const mockRows = [
+      { studentId: '1', thesisProposalTitle: 'Title1', date: '2023-01-01', status: 'Accepted' },
+      { studentId: '2', thesisProposalTitle: 'Title2', date: '2023-01-02', status: 'Rejected' },
+    ];
+  
+    component.rows = mockRows;
+    component.filteredRows = mockRows;
+  
+    component.studentFilter = '1';
+    component.titleFilter = 'Title1';
+    component.dateFilter = '2023-01-01';
+    component.statusFilter = 'Accepted';
+  
+    component.applyFilters();
+  
+    expect(component.filteredRows.length).toBe(1); // Only one row should match the applied filters
+    expect(component.filteredRows[0].studentId).toBe('1'); // The filtered row should have studentId '1'
+  });
+
+  it('should reset filters and update filteredRows', () => {
+    component.openFilter = true;
+    component.rows = [
+      { studentId: '1', thesisProposalTitle: 'Title 1', date: '2023-01-01', status: 'Accepted' },
+      { studentId: '2', thesisProposalTitle: 'Title 2', date: '2023-01-02', status: 'Rejected' },
+    ];
+
+    component.resetFilter();
+  
+    expect(component.filteredRows).toEqual(component.rows);
+    expect(component.openFilter).toBe(false);
+  });
 });
