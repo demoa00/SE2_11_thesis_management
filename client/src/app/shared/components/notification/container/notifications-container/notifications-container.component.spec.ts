5e5b34cae093cc2c8798bc91469787552942cb16
Simone Bugni Duch <simonebugniduch.SBD@gmail.com>
2023-12-15 13:59:22 +0100

    Minor fix

--- client/src/app/shared/components/notification/container/notifications-container/notifications-container.component.spec.ts
+++ client/src/app/shared/components/notification/container/notifications-container/notifications-container.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { NotificationsContainerComponent } from './notifications-container.component';
 import { Socket } from 'ngx-socket-io';
