7197557420620cef49dbc9b6edb955a369075ba4
AlessandroBelasri <belalessandro1@gmail.com>
2024-01-10 09:13:19 +0100

    update controller

--- server/controllers/ThesisRequestController.js
+++ server/controllers/ThesisRequestController.js
@@ -11,10 +11,6 @@ module.exports.getThesisRequests = async function getThesisRequests(req, res, ne
     if (checkRole.isProfessor(req.user)) {
       // get the requests for his own thesis
       thesisRequestList = await ThesisRequest.getThesisRequestsForProfessor(req.user.userId);
-<<<<<<< HEAD
-=======
-
->>>>>>> 257fb452411d385425e7fa5d801640a9791bd4f9
       utils.writeJson(res, thesisRequestList, 200);
     } else if (checkRole.isSecretaryClerck(req.user)) {
       // get all the requests
@@ -33,10 +29,6 @@ module.exports.getThesisRequestById = async function getThesisRequestById(req, r
   try {
     if (req.user != undefined && req.params.thesisRequestId != undefined) {
       let thesisRequest = await ThesisRequest.getThesisRequestById(req.user, req.params.thesisRequestId);
-<<<<<<< HEAD
-=======
-
->>>>>>> 257fb452411d385425e7fa5d801640a9791bd4f9
       utils.writeJson(res, thesisRequest, 200);
     } else {
       utils.writeJson(res, { error: "Bad Request" }, 404);
@@ -50,10 +42,6 @@ module.exports.insertNewThesisRequest = async function insertNewThesisRequest(re
   try {
     if (req.user.userId != undefined && req.body != undefined) {
       let newThesisRequest = await ThesisRequest.insertNewThesisRequest(req.user.userId, req.body);
-<<<<<<< HEAD
-=======
-
->>>>>>> 257fb452411d385425e7fa5d801640a9791bd4f9
       utils.writeJson(res, newThesisRequest, 200);
     } else {
       utils.writeJson(res, { error: "Bad Request" }, 404);
@@ -65,15 +53,9 @@ module.exports.insertNewThesisRequest = async function insertNewThesisRequest(re
 
 module.exports.updateThesisRequest = async function updateThesisRequest(req, res, next) {
   try {
-<<<<<<< HEAD
     let thesisRequestUpdated;
     if (checkRole.isProfessor(req.user)) {
       thesisRequestUpdated = await ThesisRequest.updateThesisRequestForProfessor(req.user.userId, req.body, req.params.thesisRequestId);
-=======
-    if (req.user.userId != undefined && req.body != undefined && req.params.thesisRequestId != undefined) {
-      let thesisRequestUpdated = await ThesisRequest.updateThesisRequest(req.user.userId, req.body, req.params.thesisRequestId);
-
->>>>>>> 257fb452411d385425e7fa5d801640a9791bd4f9
       utils.writeJson(res, thesisRequestUpdated, 200);
     }
     if (checkRole.isSecretaryClerck(req.user)) {
